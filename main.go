97dbfe87b0bd0a609f85e43c68b1da0a53b118e1
chris201222 <102273034+chris201222@users.noreply.github.com>
2023-12-12 19:06:30 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	msg := "Hello from Go!"
+	fmt.Fprintf(w, msg)
+
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
+
+	fmt.Println("Application started and listening on port 8080")
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		panic(err.Error())
+	}
+}
