6da37c8263e149fc47df15ee46a73742b1e37ca9
chris201222 <102273034+chris201222@users.noreply.github.com>
2023-12-18 20:31:45 +0800

    Update main.go

--- demoapp/main.go
+++ demoapp/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
-	msg := "Hello from Go!, this is a simple web server v1.0.1 build from develop branch"
+	msg := "Hello from Go!, this is a simple web server v1.0.1, Test get SHA from pull request"
 	fmt.Fprintf(w, msg)
 
 }
